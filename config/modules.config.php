8ef35a07eb63348695f5cb2469dbb310ca071364
Borton1 <gleb.igorevich777@inbox.ru>
2021-08-19 21:27:03 +0300

    first commit

--- config/modules.config.php
+++ config/modules.config.php
@@ -14,4 +14,5 @@ return [
     'Zend\Router',
     'Zend\Validator',
     'Application',
+       // <-- Add this line
 ];
--- public/index.php
+++ public/index.php
@@ -8,7 +8,10 @@ use Zend\Stdlib\ArrayUtils;
  * to the application root now.
  */
 chdir(dirname(__DIR__));
-
+if ($_SERVER['APPLICATION_ENV'] == 'development') {
+    error_reporting(E_ALL);
+    ini_set("display_errors",1);
+}
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
